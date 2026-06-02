fb7d5012c8718963ffdd8095ddf8ad7bc26e8294
Gorciu <gorciuyt@gmail.com>
2026-06-02 08:17:58 +0200

    fix(cmd/level): add missing newline

--- src/cmd/leveling/lvl.ts
+++ src/cmd/leveling/lvl.ts
@@ -53,7 +53,7 @@ const lvlCmd: Command = {
                                 row,
                                 cfg.features.leveling.levelDivider,
                             )
-                        }` +
+                        }\n` +
                         `Do kolejnego lvl: ${next_level_xp - row} XP/ok. ${Math.floor((next_level_xp - row) / cfg.features.leveling.xpPerMessage)} wiad.`,
                 )
                 .setThumbnail(who.displayAvatarURL({ size: 128 }));
