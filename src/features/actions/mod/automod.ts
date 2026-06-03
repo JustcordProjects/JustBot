0dd0a74f61be4965abbf8e6326318aee342cf557
Gorciu <gorciuyt@gmail.com>
2026-06-03 18:48:29 +0200

    feat(automod): allow for discord links in projects

--- src/features/actions/mod/automod.ts
+++ src/features/actions/mod/automod.ts
@@ -39,7 +39,7 @@ export default class AutoModRules {
         ],
         reply: (msg) => `<@${msg.author.id}> ładnie proszę, wypier*alaj ze swoją reklamą na serwery reklamowe ;)'`,
         additionalCallbacks: [PredefinedActionCallbacks.deleteMsgAutomod],
-        additionalConstraints: [AutoModRules.msgAuthorIsNotImmuneToAutomod],
+        additionalConstraints: [AutoModRules.msgAuthorIsNotImmuneToAutomod, (ctx) => ctx.channelId !== '1511773012951568434'],
     });
 
     static readonly BlockNWords: Action<MessageEventCtx> = mkAutoreplyAction({
