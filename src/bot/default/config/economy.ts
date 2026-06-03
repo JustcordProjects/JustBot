b05dea0022acf5cdf312e9e53086c6efe4b12b50
Gorciu <gorciuyt@gmail.com>
2026-06-03 08:12:43 +0200

    feat(default/configuration/economy): add income for server booster

--- src/bot/default/config/economy.ts
+++ src/bot/default/config/economy.ts
@@ -1,7 +1,24 @@
 import EconomyConfig from '@/bot/definitions/config/economy.ts';
 
 export const economyCfg: EconomyConfig = {
-    roles: [],
+    roles: [
+        {
+            name: "Server Booster",
+            desc: "Rola za boosty",
+            id: "server-booster",
+            discordRoleId: "1511602639413055569",
+            refund: 0, // have no idea what this is, ask maqix idk
+            benefits: {
+                dailyIncome: [
+                    {
+                        op: "add-money",
+                        amount: 6500
+                    }
+                ],
+                multipliers: []
+            }
+        }
+    ],
     items: [],
     offers: [],
     shop: [],
