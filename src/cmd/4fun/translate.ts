7de47454b2848415c33cfa763be3b433640cb7a0
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2026-06-04 13:38:26 +0200

    feat: add translate command

---
+++ src/cmd/4fun/translate.ts
@@ -0,0 +1,71 @@
+import { Command } from '@/bot/command.ts';
+import { CommandFlags } from '@/bot/apis/commands/misc.ts';
+import { CommandPermissions } from '@/bot/apis/commands/permissions.ts';
+
+import { translate } from '@vitalets/google-translate-api';
+import logError from '@/util/log-error.ts';
+
+function parseOpts(str: string): { from?: string; to?: string } {
+    let from: string | undefined, to: string | undefined;
+
+    if (str.includes(':')) {
+        const parts = str.split(':');
+        from = parts[0] || undefined;
+        to = parts[1] || undefined;
+    } else {
+        from = undefined;
+        to = str || undefined;
+    }
+
+    return { from, to };
+}
+
+const translateCmd: Command = {
+    name: 'translate',
+    aliases: ['tr'],
+    description: {
+        main: 'Przetłumacz tekst na jaki język ci się podoba!',
+        short: 'Tłumacz'
+    },
+
+    flags: CommandFlags.None | CommandFlags.WorksInDM,
+    permissions: CommandPermissions.everyone(),
+
+    expectedArgs: [
+        {
+            name: 'opts',
+            description: '`from:to`, lub samo `to` (wtedy `from` = auto)',
+            optional: false,
+            type: { base: 'string' }
+        },
+        {
+            name: 'text',
+            description: "Tekst który chcesz przetłumaczyć",
+            optional: false,
+            type: { base: 'string', trailing: true, allowCodeBlock: true },
+        }
+    ],
+
+    async execute(api) {
+        const optsString = api.getTypedArg('opts', 'string').value;
+        const text = api.getTypedArg('text', 'string').value;
+
+        const opts = parseOpts(optsString);
+        try {
+            const result = await translate(text, { to: opts.to, from: opts.from });
+            return api.log.replyInfo(
+                api, 'Tłumaczenie',
+                `Twój tekst w tłumaczeniu to:\n${result.text}`
+            );
+        } catch (err: unknown) {
+            logError('stdwarn', err);
+            return api.log.replyError(
+                api, 'Błąd tłumaczenia',
+                'Nie udało się przetłumaczyć twojego tekstu. Nie wiem czemu.',
+            )
+        }
+    }
+};
+
+export default translateCmd;
+
