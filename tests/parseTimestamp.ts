7727b91fd5cac2d5cca8705a9ef115a885ac0649
Gorciu <gorciuyt@gmail.com>
2026-06-30 13:49:18 +0200

    fix: git hooks

--- tests/parseTimestamp.ts
+++ tests/parseTimestamp.ts
@@ -2,12 +2,11 @@ import { assertEquals } from "@std/assert";
 import parseTimestamp from '@/util/parse-timestamp.ts';
 
 Deno.test("Timestamp parsing", () => {
-  assertEquals(parseTimestamp("10s"), 10);
-  assertEquals(parseTimestamp("2m"), 120);
-  assertEquals(parseTimestamp("1h"), 3600);
-  assertEquals(parseTimestamp("1d"), 86400);
-  assertEquals(parseTimestamp("1h30m"), 5400);
-  assertEquals(parseTimestamp("2godziny"), 7200);
-  assertEquals(parseTimestamp("42"), 42);
-  assertEquals(parseTimestamp("xyz"), null);
+    assertEquals(parseTimestamp("10s"), 10);
+    assertEquals(parseTimestamp("2m"), 120);
+    assertEquals(parseTimestamp("1h"), 3600);
+    assertEquals(parseTimestamp("1d"), 86400);
+    assertEquals(parseTimestamp("1h30m"), 5400);
+    assertEquals(parseTimestamp("42"), 42);
+    assertEquals(parseTimestamp("xyz"), null);
 });
