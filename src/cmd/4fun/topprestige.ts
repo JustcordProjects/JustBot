daed13bfdfc2a7b4263c37bf89fddf705ed4e82f
Gorciu <gorciuyt@gmail.com>
2026-07-09 12:48:10 +0200

    fix: change image in topprestige

--- src/cmd/4fun/topprestige.ts
+++ src/cmd/4fun/topprestige.ts
@@ -59,7 +59,7 @@ export default {
             embeds: [
                 new ReplyEmbed()
                     .setColor('#1ebfd5')
-                    .setImage('https://raw.githubusercontent.com/JustcordProjects/JustBot/refs/heads/main/assets/top-lvl.png'),
+                    .setImage('https://raw.githubusercontent.com/JustcordProjects/JustBot/refs/heads/main/assets/top-reputation.png'),
                 new ReplyEmbed()
                     .setFields(fields)
                     .setColor('#1ebfd5')
