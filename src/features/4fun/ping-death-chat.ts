d33c9bd3623131d8cb157f93a0fa339716befcbd
Gorciu <gorciuyt@gmail.com>
2026-09-08 17:54:53 +0200

    feat: add new death chat question list
    Signed-off-by: Gorciu <gorciuyt@gmail.com>

--- src/features/4fun/ping-death-chat.ts
+++ src/features/4fun/ping-death-chat.ts
@@ -17,41 +17,48 @@ export const pings: Record<string, PingAPI> = {
         roleId: '1511009335877046364',
         questions: [
             // life
-            'jakie filmy ostatnio oglądałeś',
-            'jakiej muzyki słuchasz',
-            'kawa czy herbata',
-            'koty czy psy',
-            'dzień czy noc',
-            'pizza czy burger',
-            'film czy serial',
-            'konsola czy PC',
+            'jaki byś film polecił do oglądania i dlaczego',
+            'jaka była ostatnia rzecz której się nauczyłeś(/-łaś (wow baby na serwerze, impossible!!!))?',
+            'czy kiedykolwiek spotkałeś się z kimś z Internetu?',
+
+            // philosophy
+            'co myślisz o mitologii greckiej?',
+            'czy wierzysz w jakąś ~~propagandę~~ **religię**?',
+            'czy, twoim zdaniem, świat ma jakiś określony cel, do któego dąży i z którego powodu w ogóle istnieje?',
+            'kto stworzył świat? a może nikt?',
+            'jaki jest sens twojego życia?',
+            'co myślisz o astral projection?',
+            'w jakie teorie spiskowe wierzysz? (ufo w area 51.. nie to słabe... ziemia jest płaska!!!!)',
 
             // maths
-            'czy matematyka została odkryta czy wymyślona',
-            'czy gdyby PI było liczbą wymierną, świat wyglądałby inaczej',
+            'jak wyglądałby świat, gdybyśmy liczyli w systemie binarnym',
+            'czy bardzo szybki algorytm na faktoryzację olbrzymich liczb całkowitych ma prawo istnieć?',
+            'jaki jest, twoim zdaniem, najtrudniejszy temat w matematyce?',
 
             // software
-            'taby czy spacje',
-            'Linux czy Windows',
-            'jakie distro linuxa wariacie',
-            'jaki był twój pierwszy język programowania',
-            'jaki język programowania najbardziej cię irytuje',
-            'co jest gorsze: PHP czy Java',
-            'ile masz otwartych tabów w przeglądarce',
-            'czy StackOverflow jeszcze żyje',
-            'umiesz czytać regexy',
-            'w jakie gry grasz',
-            'jakie projekty ostatnio robisz',
+            'dlaczego nie daily-driveujesz TempleOS?',
+            'kto pierwszy sportuje DOOMa do JUAMPa-RPG dostaje -1 rialów irańskich!!!',
+            'czy uważasz, że Linux nie powinien być POSIX-compliant?',
+            'what do you think about microkernels?',
+            'co myślisz o zbanowaniu wszystkich algorytmów szyfrujących poza szyfrem cezara? (fire pytanie wiem)',
+            'czy uważasz, że rzeczy które robił Newag z pociagami powinny być dozwolone',
 
             // hardware
-            'karty graficzne: Nvidia, AMD czy Intel',
-            'jaki producent CPU jest twoim ulubionym',
-            'ile masz RAMu; czy według ciebie to dużo czy mało',
-            'wolisz czarne czy białe podzespoły',
-            'lubisz RGB czy wolisz wyłączone',
-            'jaki rozmiar klawiatury jest najlepszy',
-            'ile portów USB masz i ile z nich używasz',
-            'ile masz monitorów a ile chciałbyś mieć',
+            'jaką masz specyfikację komputera?',
+            'jak mocny był twój pierwszy komputer?',
+            'do you care about estetyka in twój komputer wygląd?',
+            'jaka jest idealna architektura procesora? (oczywiście że powerPC64 big endian <:emoji_szczur:1511037251352526928>)',
+
+            // education
+            'czy komputery w szkole u was mają linuxa?',
+            'czy oceny 1-6 powinny być zastąpione ocenami opisowymi?',
+            'czy uważasz, że w szkole wiele się zmieniło od jakiś 100 lat?',
+            'co myślisz o przynoszeniu tabletów do szkoły zamiast zeszytów?',
+
+            // misc
+            'masz afantazję? jak nie masz to chciałbyś mieć?',
+            'co sądzisz o temacie: "Mistrzostwa Świata w Kolarstwie Torowym 1901" (nieniejszy temat was brought to you by "Losuj artykuł" on Pl.WikiPedia.org)',
+            'chciałbyś być lotnikiem / pilotem samolotu / pilotem liniowym / whatever / <wklej tu nazwę dowolnego zawodu związanego z samolotami>?'
         ],
         automatic: true,
         automaticWaitUntilLastMsgInterval: 2 * 60 * 60 * 1000,
